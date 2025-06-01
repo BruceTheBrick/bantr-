a123239e2025835a605847ca595e3d68d67e2cae
Bryce Lynch <techbryce@hotmail.com>
2025-06-01 16:21:51 +1000

    Initial Commit

---
+++ src/components/messageBubble/messageBubble.tsx
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+
+interface MessageBubbleProps {
+    message: string;
+    isCurrentUserAuthor: boolean;
+}
+
+function MessageBubble(props: MessageBubbleProps) {
+    const style = {}
+    return (
+        <div></div>
+    );
+}
+
+export default MessageBubble;
